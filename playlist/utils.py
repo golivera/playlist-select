2204a72d1943d46a1c1b13ebfdf72bbd82be89ef
Surya Pakalapati <isitnull@gmail.com>
2014-11-21 03:46:35 -0800

    Added error checking and extension checking

--- playlist/utils.py
+++ playlist/utils.py
@@ -5,6 +5,7 @@ import mydb
 
 instance = vlc.Instance()
 mediaplayer = instance.media_player_new()
+music_formats = ['.mp3','.flac','.mp4']
 
 def init_parser():
     parser = argparse.ArgumentParser()
@@ -27,14 +28,18 @@ def setup_song_db(app, music_dir=None):
     for root, dirs, files in os.walk(music_dir):
         for cur_file in files:
             file_path = os.path.abspath(os.path.join(root, cur_file))
-            # TODO: Error checking for correct music formats
-            mydb.insert_song(cur_file, file_path)
+            if os.path.splitext(cur_file) in music_formats:
+                mydb.insert_song(cur_file, file_path)
 
 
 # VLC Utility Methods
-def play_song():
-    # TODO: Check File Exists
-    # TODO: Get top of song queue
+def play_song(filepath):
+
+    if not filepath or not os.path.isfile(filepath):
+        return
+
+    print "Playing: {}".format(filepath)
+
     media = instance.media_new(unicode(filepath))
     mediaplayer.set_media(media)
     mediaplayer.play()
