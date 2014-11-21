f0438161b222acc5846c5353ee895cbb42f13c00
G O <gary21490@gmail.com>
2014-11-20 23:09:56 -0800

    Setup test folder and basic dir walking

--- playlist/playlist.py
+++ playlist/playlist.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+
 from flask import Flask, session, render_template, request
 from contextlib import closing
 
@@ -30,10 +32,19 @@ def connect_db():
 
 def init_db():
     with connect_db() as db:
-	with app.open_resource('schema.sql', mode='r') as f:
-	     db.cursor().executescript(f.read())
-	db.commit()		
+	    with app.open_resource('schema.sql', mode='r') as f:
+	        db.cursor().executescript(f.read())
+    db.commit()
+
+def setup_song_db():
+    print "Setting up song DB"
+    for root, dirs, files in os.walk('test_music'):
+        for cur_file in files:
+            file_path = os.path.abspath(os.path.join(root, cur_file))
+            print file_path
 
 if __name__ == '__main__':
     app.debug = True
+    setup_song_db()
     app.run()
+
